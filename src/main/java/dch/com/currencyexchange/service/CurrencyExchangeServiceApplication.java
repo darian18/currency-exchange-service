80113d7bb290f21fcf0fe56e23069296d8e1ee06
Dariusz Chmieła <dariusz.chmiela@ipfdigital.com>
2020-05-11 23:38:25 +0200

    Initial commit + project skeleton

---
+++ src/main/java/dch/com/currencyexchange/service/CurrencyExchangeServiceApplication.java
@@ -0,0 +1,13 @@
+package dch.com.currencyexchange.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dch/com/currencyexchange/service/CurrencyExchangeControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package dch.com.currencyexchange.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
