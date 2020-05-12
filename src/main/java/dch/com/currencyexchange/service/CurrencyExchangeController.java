11ba42062adedba75e3c9f657c38ee2e2fce35cf
Dariusz Chmieła <dariusz.chmiela@ipfdigital.com>
2020-05-12 15:27:32 +0200

    MSA: Applied code review remarks to CurrencyExchangeController

--- src/main/java/dch/com/currencyexchange/service/CurrencyExchangeController.java
+++ src/main/java/dch/com/currencyexchange/service/CurrencyExchangeController.java
@@ -1,24 +1,26 @@
 package dch.com.currencyexchange.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
+import java.util.Objects;
 
 @RestController
 public class CurrencyExchangeController {
-    @Autowired
-    private Environment environment;
-    @Autowired
-    private ExchangeValueRepository exchangeValueRepository;
+    private final Environment environment;
+    private final ExchangeValueRepository exchangeValueRepository;
+
+    public CurrencyExchangeController(Environment environment, ExchangeValueRepository exchangeValueRepository) {
+        this.environment = environment;
+        this.exchangeValueRepository = exchangeValueRepository;
+    }
 
     @GetMapping("currency-exchange/from/{from}/to/{to}")
     public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to){
         ExchangeValue exchangeValue = exchangeValueRepository.findByFromAndTo(from, to);
-        exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+        exchangeValue.setPort(Integer.parseInt(Objects.requireNonNull(environment.getProperty("local.server.port"))));
         return exchangeValue;
     }
 }
